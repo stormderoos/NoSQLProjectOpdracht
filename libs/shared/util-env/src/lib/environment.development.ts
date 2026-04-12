165901570e5e24d1eea5aa92bdd28a3ab793904f
stormderoos <sw.deroos@student.avans.nl>
2026-04-12 17:38:21 +0200

    env correct

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -8,6 +8,6 @@ export const environment: IEnvironment = {
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://swderoos:swWelkom01!@footballdbcluster.vsocs.mongodb.net/',
     NEO4J_URI: 'neo4j+s://98870026.databases.neo4j.io',
-    NEO4J_USER: 'neo4j',
+    NEO4J_USER: '98870026',
     NEO4J_PASSWORD: '8eJ-M_l051tvUwhWrlGnIDCRXx1aHI02fcI7jJaQoEE',
 };
