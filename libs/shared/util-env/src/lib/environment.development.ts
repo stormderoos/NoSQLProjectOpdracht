266b67584893f107a5cfb98682466369d80a7503
Storm Rose <storm.deroos05@gmail.com>
2026-04-21 14:55:48 +0200

    chore: update Neo4j credentials to new AuraDB instance (a4465224)

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -7,8 +7,8 @@ export const environment: IEnvironment = {
     dataApiUrl: 'http://localhost:3000/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://swderoos:swWelkom01!@footballdbcluster.vsocs.mongodb.net/',
-    NEO4J_URI: 'neo4j+s://98870026.databases.neo4j.io',
-    NEO4J_USER: '98870026',
-    NEO4J_PASSWORD: '8eJ-M_l051tvUwhWrlGnIDCRXx1aHI02fcI7jJaQoEE',
-    NEO4J_DATABASE: '98870026',
+    NEO4J_URI: 'neo4j+s://a4465224.databases.neo4j.io',
+    NEO4J_USER: 'neo4j',
+    NEO4J_PASSWORD: 'CYFsQmVLFkGjKyHwkhb9kGZO93z31C3fvGKwGc2n3os',
+    NEO4J_DATABASE: 'neo4j',
 };
